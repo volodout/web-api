463afd9b714c17de59ca72f81d464abb1dffb82a
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-12 16:55:21 +0500

    GetUserById

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -8,15 +8,34 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
+    private readonly IUserRepository userRepository;
+
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
     public UsersController(IUserRepository userRepository)
     {
+        this.userRepository = userRepository;
     }
 
     [HttpGet("{userId}")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var userEntity = userRepository.FindById(userId);
+        if (userEntity == null)
+            return NotFound();
+        var userDto = ConvertToDto(userEntity);
+        return Ok(userDto);
+    }
+
+    private UserDto ConvertToDto(UserEntity userEntity)
+    {
+        return new UserDto
+        {
+            Id = userEntity.Id,
+            Login = userEntity.Login,
+            FullName = $"{userEntity.LastName} {userEntity.FirstName}",
+            GamesPlayed = userEntity.GamesPlayed,
+            CurrentGameId = userEntity.CurrentGameId
+        };
     }
 
     [HttpPost]

7a0ceace3075d266a0d8cb6889aac6b14b735d0c
mmdoneperson <mohmaks220@gmail.com>
2025-10-12 21:34:06 +0500

    feat: add 4, 5, 6 task

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
@@ -20,6 +22,7 @@ public class UsersController : Controller
     }
 
     [HttpGet("{userId}", Name = nameof(GetUserById))]
+    [HttpHead("{userId}")]
     [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
@@ -27,6 +30,11 @@ public class UsersController : Controller
         if (userEntity == null)
             return NotFound();
         var userDto = mapper.Map<UserDto>(userEntity);
+        Response.ContentType = "application/json; charset=utf-8";
+        if (HttpMethods.IsHead(Request.Method))
+        {
+            return Ok();
+        }
         return Ok(userDto);
     }
 
@@ -73,6 +81,57 @@ public class UsersController : Controller
                 routeValues: new { userId },
                 value: userId);
 
+        return NoContent();
+    }
+    
+    [HttpPatch("{userId}", Name = nameof(PatchUser))]
+    [Consumes("application/json")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult PatchUser([FromRoute] Guid userId, [FromBody] JsonPatchDocument<UserPatchDto>? patchDoc)
+    {
+        if (patchDoc is null)
+            return BadRequest();
+        if (userId == Guid.Empty)
+            return NotFound();
+        var userEntity = userRepository.FindById(userId);
+        if (userEntity is null)
+            return NotFound();
+        var userPatch = new UserPatchDto
+        {
+            Login = userEntity.Login,
+            FirstName = userEntity.FirstName,
+            LastName = userEntity.LastName
+        };
+        patchDoc.ApplyTo(userPatch, ModelState);
+        if (string.IsNullOrEmpty(userPatch.Login))
+            ModelState.AddModelError("login", "login is required");
+        else if (!userPatch.Login.All(char.IsLetterOrDigit))
+            ModelState.AddModelError("login", "login must be alphanumeric");
+        if (userPatch.LastName is null || userPatch.LastName.Length == 0)
+            ModelState.AddModelError("lastName", "length of the last name must be greater than 0");
+        if (userPatch.FirstName is not null && userPatch.FirstName.Length == 0)
+            ModelState.AddModelError("firstName", "length of the first name must be greater than 0");
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        if (userPatch.Login != null)
+            userEntity.Login = userPatch.Login;
+        if (userPatch.FirstName != null)
+            userEntity.FirstName = userPatch.FirstName;
+        if (userPatch.LastName != null)
+            userEntity.LastName = userPatch.LastName;
+        userRepository.Update(userEntity);
+        return NoContent();
+    }
+
+    [HttpDelete("{userId}", Name = nameof(DeleteUser))]
+    public IActionResult DeleteUser([FromRoute] Guid userId)
+    {
+        if (userId == Guid.Empty)
+            return NotFound();
+        var userEntity = userRepository.FindById(userId);
+        if (userEntity is null)
+            return NotFound();
+        userRepository.Delete(userId);
         return NoContent();
     }
 }
---
+++ WebApi.MinimalApi/Models/UserPatchDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.MinimalApi.Models;
+
+public class UserPatchDto
+{
+    public string? Login { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}

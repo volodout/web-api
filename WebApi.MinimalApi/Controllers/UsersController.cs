1a7382ea51a1de3009ebf38947b6f075fa387bf9
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-12 17:15:40 +0500

    Auto mapper

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -9,33 +10,24 @@ namespace WebApi.MinimalApi.Controllers;
 public class UsersController : Controller
 {
     private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
 
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         this.userRepository = userRepository;
+        this.mapper = mapper;
     }
 
     [HttpGet("{userId}")]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
         var userEntity = userRepository.FindById(userId);
         if (userEntity == null)
             return NotFound();
-        var userDto = ConvertToDto(userEntity);
+        var userDto = mapper.Map<UserDto>(userEntity);
         return Ok(userDto);
-    }
-
-    private UserDto ConvertToDto(UserEntity userEntity)
-    {
-        return new UserDto
-        {
-            Id = userEntity.Id,
-            Login = userEntity.Login,
-            FullName = $"{userEntity.LastName} {userEntity.FirstName}",
-            GamesPlayed = userEntity.GamesPlayed,
-            CurrentGameId = userEntity.CurrentGameId
-        };
     }
 
     [HttpPost]
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,12 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
 using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+    options.RespectBrowserAcceptHeader = true;
+})
+    .ConfigureApiBehaviorOptions(options =>
+    {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
     });
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+}, new System.Reflection.Assembly[0]);
+
 builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 
 var app = builder.Build();
